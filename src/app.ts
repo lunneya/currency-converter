97620fdc64c07ea65a140e47a7e738d46ff5aa27
lunneya <lunneya.nivarre@gmail.com>
2025-07-28 23:56:24 +0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+// 1 яблоко стоит 5 груш.
+// 1 арбуз стоит 8 груш.
+// Сколько яблок стоит 1 арбуз?
+
+// let apple: number = ;
+// let j: number = 8;
+//
+// let result: number = ruble / j;
+//
+// console.log(result);
+
