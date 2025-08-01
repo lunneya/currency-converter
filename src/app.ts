f14e570e031808e5256e27e48a469338af71482c
lunneya <lunneya.nivarre@gmail.com>
2025-08-01 22:11:45 +0500

    start work with api

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-// 1 яблоко стоит 5 груш.
-// 1 арбуз стоит 8 груш.
-// Сколько яблок стоит 1 арбуз?
+(async()=>{
+    const getData = async() => {
+        const data = await fetch('https://api.nbp.pl/api/exchangerates/rates/a/gbp/2012-01-02/')
+        const result = await data.json()
+        return result
+    }
 
-// let apple: number = ;
-// let j: number = 8;
-//
-// let result: number = ruble / j;
-//
-// console.log(result);
-
+    const data = await getData()
+    console.log(data)
+})()
