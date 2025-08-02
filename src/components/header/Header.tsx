e37ea50a9e4b965eb9b9faeb6d8baf99f33d9398
lunneya <lunneya.nivarre@gmail.com>
2025-08-02 20:08:28 +0500

    added react to the project and rewrote it into components

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+// @ts-ignore
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="header-title">Converter</h1>
+        </header>
+    );
+}
+
+export default Header;
